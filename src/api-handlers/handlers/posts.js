48682d6d97f0ed7230cd1718bde56fbf9bcaa816
Nitin <nitindoodhiya.com>
2023-12-09 18:35:54 +0530

    validate input

--- src/api-handlers/handlers/posts.js
+++ src/api-handlers/handlers/posts.js
@@ -2,11 +2,16 @@ const { UserError } = require("../../modules/errors");
 
 const response = (object) => res.status(200).json(object);
 
-module.exports = async (req, res) => {
-  const { id, content } = req?.body;
+function validateInput(id, content) {
   if (!id || !content) {
     return response(UserError("Empty Input Received", 400));
   }
+}
+
+module.exports = async (req, res) => {
+  const { id, content } = req?.body;
+  const error = validateInput(id, content);
+  if (!error) return error;
 
   return response({ result: 1 });
 };
