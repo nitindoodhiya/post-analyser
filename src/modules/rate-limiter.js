c1fc7c753e0a72d76cde16954144d4278fe10742
Nitin <nitindoodhiya.com>
2023-12-09 23:28:48 +0530

    set expire time when requested for the first time

--- src/modules/rate-limiter.js
+++ src/modules/rate-limiter.js
@@ -5,12 +5,13 @@ async function rateLimiter(req, res, next) {
   const timestamp = Math.floor(new Date() / 1000);
   const ip = req.socket.remoteAddress;
   const key = `${ip}_${timestamp}`;
-  const currentQPS = await redis.incr(key);
 
-  console.log({ currentQPS, key });
+  const currentQPS = await redis.incr(key);
+  if (currentQPS === 1) await redis.setKey(key, 1);
   if (currentQPS >= limit) {
-    res.status(429).json({ errMessage: "Too Many Requests" });
-  } else return next();
+    return res.status(429).json({ errMessage: "Too Many Requests" });
+  }
+  return next();
 }
 
 module.exports = rateLimiter;
